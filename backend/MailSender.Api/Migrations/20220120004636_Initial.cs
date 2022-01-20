b669ca649178310e6e38e3b963a7a98736e30c82
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-01-19 21:47:55 -0300

    feat: add initial migrations

---
+++ backend/MailSender.Api/Migrations/20220120004636_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MailSender.Api.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MailSender",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subject = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: true),
+                    Destiny = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MailSender", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MailSender");
+        }
+    }
+}
