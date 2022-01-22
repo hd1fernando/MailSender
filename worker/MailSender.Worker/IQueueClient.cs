1627e567c40e0890388ffbed1f1b1d5367005859
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-01-21 21:23:48 -0300

    feat: add Rabbitmq to worker

---
+++ worker/MailSender.Worker/IQueueClient.cs
@@ -0,0 +1,4 @@
+public interface IQueueClient
+{
+    public void ReadQueue();
+}
---
+++ worker/MailSender.Worker/QueueClient.cs
@@ -0,0 +1,38 @@
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class QueueClient : IQueueClient
+{
+    public void ReadQueue()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "123456"
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [[{0}] Received {1}", DateTime.Now, message);
+        };
+        channel.BasicConsume(queue: "mail_sender",
+                             autoAck: true,
+                             consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
