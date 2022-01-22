68d7416aa296eb979a603704273ac2cc67518a7c
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-01-21 21:22:25 -0300

    format QueueServices code

--- backend/MailSender.Api/Services/QueueServices.cs
+++ backend/MailSender.Api/Services/QueueServices.cs
@@ -27,19 +27,13 @@ public class QueueServices : IQueueServices
 
         using var connection = factory.CreateConnection();
         using var channel = connection.CreateModel();
-        channel.QueueDeclare(queue: "mail_sender",
-                             durable: false,
-                             exclusive: false,
-                             autoDelete: false,
-                             arguments: null);
+        channel.QueueDeclare(queue: "mail_sender", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
         string message = JsonSerializer.Serialize(mailEntity);
         var body = Encoding.UTF8.GetBytes(message);
 
-        channel.BasicPublish(exchange: "",
-                             routingKey: "mail_sender",
-                             basicProperties: null,
-                             body: body);
+        channel.BasicPublish(exchange: "", routingKey: "mail_sender", basicProperties: null, body: body);
+        
         Console.WriteLine($" [{DateTime.Now}] Sent {message}");
     }
 }
