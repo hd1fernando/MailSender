07f486546735beebb40fcb6ba9e9d19b69b3b958
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-01-30 21:46:11 -0300

    feat: add logs in api

--- backend/MailSender.Api/Services/QueueServices.cs
+++ backend/MailSender.Api/Services/QueueServices.cs
@@ -10,14 +10,19 @@ namespace MailSender.Api.Services;
 public class QueueServices : IQueueServices
 {
     private readonly IOptions<RabbitMQOptions> RabbitMqOptions;
-
-    public QueueServices(IOptions<RabbitMQOptions> rabbitMqOptions)
+    private readonly ILogger<QueueServices> Logger;
+    public QueueServices(IOptions<RabbitMQOptions> rabbitMqOptions, ILogger<QueueServices> logger)
     {
         RabbitMqOptions = rabbitMqOptions;
+        Logger = logger;
     }
 
     public void AddToQueue(MailEntity mailEntity)
     {
+        Logger.LogInformation("Connecting with: {0},{1},{2}", RabbitMqOptions.Value.HostName, RabbitMqOptions.Value.UserName, RabbitMqOptions.Value.Password);
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+
+        Logger.LogInformation("Starting RabbitMQ connection.");
         var factory = new ConnectionFactory()
         {
             HostName = RabbitMqOptions.Value.HostName,
@@ -33,8 +38,9 @@ public class QueueServices : IQueueServices
         string message = JsonSerializer.Serialize(mailEntity);
         var body = Encoding.UTF8.GetBytes(message);
 
+        Logger.LogInformation("Publishing message.");
         channel.BasicPublish(exchange: "", routingKey: "mail_sender", basicProperties: null, body: body);
 
-        Console.WriteLine($" [{DateTime.Now}] Sent {message}");
+        Logger.LogInformation($" [{DateTime.UtcNow}] Sent {message}.");
     }
 }
